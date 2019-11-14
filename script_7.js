974bb15e88a0fbcd4a1681630b4765eeca952386
Megane <fulconis.megane@gmail.com>
2019-11-14 21:18:30 +0100

    finito

--- script_7.js
+++ script_7.js
@@ -1,7 +1,23 @@
-let answer = "";
-while(answer !== "oui") {
-  console.log("alors ?")
-  answer = prompt("Tu kiffes THP ?");
-} 
+ /*global console*/ /* eslint no-console: "off" */
 
-console.log("haaa, tu nous fais plaisir !") // Qui se lassera le premier entre toi et ton ordi ? :D
+
+answer = prompt("test")
+
+
+//message vide
+if (answer == ""){
+	console.log("t'es en PLS ?")}
+
+//message avec fortnite
+else if (answer.includes("fortnite")){
+	console.log("on s' fait une partie soum-soum ?")}
+
+//message en majuscule
+else if (answer == answer.toUpperCase()) {
+	console.log("Pwa, calme-toi...")
+}
+
+//message qui finit par ? 
+
+// sans condition
+else { console.log("balek")};
